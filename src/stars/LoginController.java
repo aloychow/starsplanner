d8a0432ef09cdfedbff453ec000319d8f4cec875
Aratrika Pal <dr.biplabpal@gmail.com>
2020-11-05 18:07:02 +0800

    initial commit

---
+++ src/stars/LoginController.java
@@ -0,0 +1,8 @@
+package stars;
+
+public class LoginController {
+    public boolean validateLogin(String enteredEmail, String enteredPassword, String typeOfUser)
+    {
+        return true;
+    }
+}
---
+++ src/stars/StudentModeUI.java
@@ -0,0 +1,60 @@
+package stars;
+
+import java.util.Scanner;
+
+public class StudentModeUI implements PrintMenuUI {
+    public void showMenu(User user) {
+        //TODO
+        int choice;
+        Scanner sc = new Scanner(System.in);
+        StudentModeController smc = new StudentModeController();
+
+        do {
+            System.out.println("");
+            System.out.println("");
+            System.out.println("Welcome to STARS! You may perform the following Actions:");
+            System.out.println("--------------------------------------------------------");
+            System.out.println("1: Add a Course");
+            System.out.println("2: Drop a Course");
+            System.out.println("3: Print Registered Courses");
+            System.out.println("4: Check Vacancies");
+            System.out.println("5: Change Index Number");
+            System.out.println("6: Swap Index Number");
+            System.out.println("7: Check Access Period");
+            System.out.println("8: Quit");
+            System.out.print("-------Please Enter your choice:");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    smc.addCourse();
+                    break;
+                case 2:
+                    smc.dropCourse();
+                    break;
+                case 3:
+                    smc.printRegisteredCourses();
+                    break;
+                case 4:
+                    smc.checkVacanciesAvailable();
+                    break;
+                case 5:
+                    smc.changeIndexNumber();
+                    break;
+                case 6:
+                    smc.swapIndexnumber();
+                    break;
+                case 7:
+                    smc.checkAccessPeriod();
+                    break;
+                case 8:
+                    System.out.println("Program terminating Ö.");
+                    break;
+                default:
+                    System.out.println("Error!");
+                    break;
+            }
+        } while (choice != 8);
+    }
+
+}
