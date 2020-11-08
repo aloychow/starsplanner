f6e76916679c4ef88cf30c1f4cc8faa562d89233
Aratrika Pal <dr.biplabpal@gmail.com>
2020-11-08 22:48:47 +0800

    added Email class

---
+++ src/stars/Email.java
@@ -0,0 +1,13 @@
+package stars;
+import java.util.*;
+
+
+public class Email {
+    private static final String USERNAME = "ntuStars";  // GMail user name (just the part before "@gmail.com")
+    private static String PASSWORD = "stars1234"; // GMail password
+    private static String RECIPIENT = "aratrikapal510@gmail.com";
+    public void sendEmail(String subject, String text, String recipient)
+    {
+        
+    }
+}
